8823e9881c79601d93c4219f182c7d2e57426abb
Gleb Fedotov <innercoding@gmail.com>
2023-08-04 18:12:03 +0300

    switch colors to catppuccin

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "monokai-pro",
+  -- colorscheme = "monokai-pro",
+  colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.angular" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
